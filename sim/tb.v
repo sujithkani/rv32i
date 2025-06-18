803d0dbaa92910bb035765ce9b978c254eece7b4
Sujith Kani <sujithkani2004@gmail.com>
2025-06-18 11:35:33 +0530

    Update tb.v

--- sim/tb.v
+++ sim/tb.v
@@ -1,30 +1,18 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 module tb;
+  reg clk=0, rst=1;
 
-  reg clk = 0;
-  reg rst = 1;
-
-  rv32i_top dut (
-    .clk(clk),
-    .rst(rst)
-  );
-
-  always #5 clk = ~clk;
-
+  rv32i_top dut(.clk(clk),.rst(rst));
+  always #5 clk=~clk;
+  
   initial begin
     $dumpfile("rv32i.vcd");
-    $dumpvars(0, dut);
-
-    #20 rst = 0;
-
-    // Allow enough time for execution
-    #980;
-
+    $dumpvars(0,dut);
+    #20 rst=0;
+    // Allow enough time
+    #1000;
     // Check memory content at address 0 (result of sw x3, 0(x0))
-    $display("MEM[0] = %h", dut.data_mem.mem[0]);
-
-
+    $display("MEM[0]=%h",dut.data_mem.mem[0]);
     #1 $finish;
   end
-
 endmodule
